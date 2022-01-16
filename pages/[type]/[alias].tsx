6584fa93f19ad71f0759053fc79f19794b6f3390
Yuri <ykuzm@mail.ru>
2022-01-16 16:11:58 +0300

    fix: undefind ref

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -16,21 +16,28 @@ import Head from 'next/head';
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
 	return (
 		<>
-			<Head>
-				<title>{page.metaTitle}</title>
-				<meta name='dedcription' content={page.metaDescription} />
-				<meta property='og:title' content={page.metaTitle} />
-				<meta
-					property='og:dedcription'
-					content={page.metaDescription}
-				/>
-				<meta property='og:type' content='article' />
-			</Head>
-			<TopPageComponent
-				firstCategory={firstCategory}
-				page={page}
-				products={products}
-			/>
+			{page && products && (
+				<>
+					<Head>
+						<title>{page.metaTitle}</title>
+						<meta
+							name='dedcription'
+							content={page.metaDescription}
+						/>
+						<meta property='og:title' content={page.metaTitle} />
+						<meta
+							property='og:dedcription'
+							content={page.metaDescription}
+						/>
+						<meta property='og:type' content='article' />
+					</Head>
+					<TopPageComponent
+						firstCategory={firstCategory}
+						page={page}
+						products={products}
+					/>
+				</>
+			)}{' '}
 		</>
 	);
 }
